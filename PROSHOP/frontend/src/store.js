4feda09afe7392267bb4ff08484c93f66d270b5b
ulvi macid <ulvizm@code.edu.az>
2021-02-05 15:35:28 +0400

    adding product with product Reducer

--- PROSHOP/frontend/src/store.js
+++ PROSHOP/frontend/src/store.js
@@ -1,11 +1,12 @@
 import {createStore,combineReducers,applyMiddleware} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import {productListReducer} from "./reducers/productReducers"
+import {productListReducer,productDetailReducer} from "./reducers/productReducers"
 
 
 const reducer=combineReducers({
-    productList:productListReducer
+    productList:productListReducer,
+    productDetail:productDetailReducer
 });
 const middleware=[thunk];
 const initialState={};
