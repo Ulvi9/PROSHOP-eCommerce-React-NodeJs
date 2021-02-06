0750281e045e8c52ed82268957cccabe38018af3
ulvi macid <ulvizm@code.edu.az>
2021-02-06 14:13:34 +0400

    adding productController(backend)

---
+++ PROSHOP/backend/controllers/productController.js
@@ -0,0 +1,25 @@
+import Product from "../models/productModel.js";
+import asyncHandler from "../middleware/async.js";
+import ErrorResponse from "../utils/errorResponse.js";
+
+
+//@desc get all products
+//@route GET api/products/courses
+//@access public
+const getAllProducts=asyncHandler(async(req,res,next)=>{
+    const products=await Product.find({});
+    //throw new ErrorResponse("error",404)
+    res.json(products);
+});
+
+//@desc get  product
+//@route GET api/products/product/:id
+//@access public
+const getProduct=asyncHandler(async(req,res,next)=>{
+    const product=await Product.findById(req.params.id);
+    if (!product){
+        return  next(new ErrorResponse(`product did not find with id ${req.params.id}`),404) ;
+    };
+    res.json(product);
+})
+export { getProduct,getAllProducts}
--- PROSHOP/backend/routes/productRoutes.js
+++ PROSHOP/backend/routes/productRoutes.js
@@ -1,27 +1,10 @@
 import express from "express";
-import Product from "../models/productModel.js";
-// import asyncHandler from "express-async-handler";
-import asyncHandler from "../middleware/async.js";
-import ErrorResponse from "../utils/errorResponse.js";
+import {
+    getAllProducts,
+    getProduct}
+    from "../controllers/productController.js";
+
 const router=express.Router();
-
-//@desc get all products
-//@route GET api/products/courses
-//@access public
-router.get("/",asyncHandler(async (req, res) => {
-    const products=await Product.find({});
-    //throw new ErrorResponse("error",404)
-    res.json(products);
-}))
-//@desc get  product
-//@route GET api/products/product/:id
-//@access public
-router.get("/:id",asyncHandler(async (req, res,next) => {
-    const product=await Product.findById(req.params.id);
-    if (!product){
-       return  next(new ErrorResponse(`product did not find with id ${req.params.id}`),404) ;
-    };
-     res.json(product);
-
-}))
+router.route("/").get(getAllProducts);
+router.route("/:id").get(getProduct);
 export default router;
