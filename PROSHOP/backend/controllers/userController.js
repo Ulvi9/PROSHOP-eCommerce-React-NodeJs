489c5038e3460b2fa31b63b1a111a758e67713c8
ulvi macid <ulvizm@code.edu.az>
2021-02-07 12:59:46 +0400

    adding user Profile(backend)

--- PROSHOP/backend/controllers/userController.js
+++ PROSHOP/backend/controllers/userController.js
@@ -56,6 +56,10 @@ const register=asyncHandler(async (req,res,next)=>{
         return next(new ErrorResponse("Invalid user data",400));
     }
 });
+
+//@desc get user profile
+//@route GET api/users/profile
+//@access public
 const getUserProfile=asyncHandler(async(req,res,next)=>{
     // user is already available in req due to the protect middleware
     // const user = req.user;
@@ -74,4 +78,32 @@ const getUserProfile=asyncHandler(async(req,res,next)=>{
     }
 });
 
-export {auth,getUserProfile,register}
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id),
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+export {auth,getUserProfile,register,updateUserProfile}
--- PROSHOP/backend/routes/userRoutes.js
+++ PROSHOP/backend/routes/userRoutes.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
     auth,
     getUserProfile,
-    register}
+    register,
+    updateUserProfile}
     from "../controllers/userController.js";
 import {protect} from "../middleware/authMiddleware.js";
 
@@ -11,4 +12,5 @@ const router=express.Router();
 router.post("/login",auth);
 router.route("/profile").get(protect,getUserProfile);
 router.route("/register").post(register);
+router.route("/update").put(protect,updateUserProfile);
 export default router;
